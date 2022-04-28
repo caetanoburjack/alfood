c7753cac48af63792f5380a61ecf117603c5c75e
Caetano Burjack <caetano.burjack@gmail.com>
2022-04-28 15:53:29 -0300

    Criando paginação padrão

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -94,6 +94,7 @@ const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
+  const [paginaAnterior, setPaginaAnterior] = useState('');
 
   useEffect(() => {
     axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
@@ -106,6 +107,15 @@ const ListaRestaurantes = () => {
       })
   }, [])
 
+  const carregarDados = (url: string) => {
+    axios.get<IPaginacao<IRestaurante>>(url)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
+      })
+  }
+
   const verMais = () => {
     axios.get<IPaginacao<IRestaurante>>(proximaPagina)
       .then(resposta => {
@@ -117,6 +127,13 @@ const ListaRestaurantes = () => {
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={verMais}>Ver Mais </button>}
+
+    {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
+      Página Anterior
+    </button>}
+    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
+      Próxima página
+    </button>}
   </section>)
 }
 
