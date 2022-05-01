cd00040e4c998c3dc0a60c66318bbd8f896d4250
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-01 00:09:23 -0300

    renomeando componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
 import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
-import FormularioPrato from './paginas/Administracao/Pratos/FormularioRestaurante';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import Home from './paginas/Home';
--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Paper, TextField, Typography } from "@mui/material"
+import { Box, Button, Card, CardActions, CardContent, CardMedia, TextField, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import http from "../../../http"
